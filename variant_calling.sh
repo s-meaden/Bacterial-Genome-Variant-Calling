566c13d6af975d63021f887545242c627c64b024
Unknown <s.meaden@exeter.ac.uk>
2017-11-07 17:14:45 +0000

    Edited commented out lines

--- variant_calling.sh
+++ variant_calling.sh
@@ -9,20 +9,20 @@
 # Map to reference w/ BWA-MEM
 
 ref=Pseudomonas_aeruginosa_UCBPP-PA14_109.fna
-#bwa index $ref
+bwa index $ref
 
 # Folder for alignments:
-#mkdir 20170801_Meaden1/alignments/
+mkdir 20170801_Meaden1/alignments/
 
 # Loop through each sample (extract basename)
 # (not using reads that lost a mate during trimming)
-#for sample in `ls 20170801_Meaden1/reads/*_1_trimmed.fastq.gz`
-#do
-#dir="20170801_Meaden1/alignments/"
-#base=$(basename $sample "_1_trimmed.fastq.gz")
-#bwa mem -t 16 $ref 20170801_Meaden1/reads/${base}_1_trimmed.fastq.gz 20170801_Meaden1/reads/${base}_2_trimmed.fastq.gz > ${dir}/${base}.sam
-#echo $base
-#done
+for sample in `ls 20170801_Meaden1/reads/*_1_trimmed.fastq.gz`
+do
+dir="20170801_Meaden1/alignments/"
+base=$(basename $sample "_1_trimmed.fastq.gz")
+bwa mem -t 16 $ref 20170801_Meaden1/reads/${base}_1_trimmed.fastq.gz 20170801_Meaden1/reads/${base}_2_trimmed.fastq.gz > ${dir}/${base}.sam
+echo $base
+done
 
 # Convert SAM to BAM
 for sample in 20170801_Meaden1/alignments/*.sam
@@ -30,36 +30,36 @@ do
 echo $sample
 describer=$(echo ${sample} | sed 's/.sam//')
 echo $describer
-#samtools view -bT $ref $sample > ${describer}.uns.bam
+samtools view -bT $ref $sample > ${describer}.uns.bam
 
 # Sort BAM file
-#samtools sort ${describer}.uns.bam ${describer}
+samtools sort ${describer}.uns.bam ${describer}
 
 # Index bam file
-#samtools index ${describer}.bam
+samtools index ${describer}.bam
 
 # Remove intermediates:
-#rm ${describer}.uns.bam
+rm ${describer}.uns.bam
 
 # Remove duplicate reads. Samtools and picard interchangeable for these steps.
 # Using picard for compatability w/ GATK HaplotypeCaller (although both should work)
 
-#~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/picard.jar MarkDuplicates INPUT=${describer}.bam OUTPUT=${describer}_dedup.bam METRICS_FILE=${describer}_metrics.txt
+~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/picard.jar MarkDuplicates INPUT=${describer}.bam OUTPUT=${describer}_dedup.bam METRICS_FILE=${describer}_metrics.txt
 done
 
 # Change headers so compatible w/ HaplotypeCaller
-#for i in 20170801_Meaden1/alignments/*dedup.bam
-#do
-#~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/picard.jar AddOrReplaceReadGroups I=${i} O=${i}_renamed.bam RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=${i}
+for i in 20170801_Meaden1/alignments/*dedup.bam
+do
+~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/picard.jar AddOrReplaceReadGroups I=${i} O=${i}_renamed.bam RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=${i}
 
 # Index new BAM files:
-#~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/picard.jar BuildBamIndex INPUT=${i}_renamed.bam
-#done
+~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/picard.jar BuildBamIndex INPUT=${i}_renamed.bam
+done
 
 # Create sequence dictionary:
-#~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/picard.jar CreateSequenceDictionary R=$ref O=${ref}.dict
+~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/picard.jar CreateSequenceDictionary R=$ref O=${ref}.dict
 
-# Call SNPs w/ HaplotypeCaller. Need to input all BAM files separately?
+# Call SNPs w/ HaplotypeCaller. Make sure list of BAM files includes path
 ~/programs/jdk1.8.0_151/jre/bin/java -jar ~/programs/GenomeAnalysisTK.jar \
   -T HaplotypeCaller \
   -R $ref \
